2f089896c92d9f623bb57adf73f9d4b5789a20bc
Rytis Kaplūnas <rytkaplunas@gmail.com>
2017-01-29 16:24:29 +0200

    Init project

---
+++ ChangeThatTrackApp/ChangeThatTrack/ConnectionHandler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
+namespace ChangeThatTrack
+{
+    public class ConnectionHandler
+    {
+        TcpClient tcpclnt;
+        
+        public string SongInfo
+        {
+            get;
+            set;
+        }
+
+        public ConnectionHandler()
+        {
+            tcpclnt = new TcpClient();
+            try
+            {
+
+                Console.WriteLine("Connecting.....");
+
+                tcpclnt.Connect("172.16.12.210", 8001);
+                // use the ipaddress as in the server program
+
+                Console.WriteLine("Connected");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error..... " + ex.StackTrace);
+            }
+        }
+        public void Send(int code)
+        {
+            Stream stm = tcpclnt.GetStream();
+
+            byte[] ba = BitConverter.GetBytes(code);
+            Console.WriteLine("Transmitting.....");
+
+            stm.Write(ba, 0, ba.Length);
+
+            byte[] bb = new byte[100];
+            int k = stm.Read(bb, 0, 100);
+
+            Console.WriteLine("**************************");
+            for (int i = 0; i < k; i++)
+                Console.Write(Convert.ToChar(bb[i]));
+            Console.WriteLine("");
+            Console.WriteLine("**************************");
+        }
+
+        public void CloseConnection()
+        {
+            tcpclnt.Close();
+        }
+    }
+}
---
+++ ChangeThatTrackApp/ChangeThatTrack/MainActivity.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ChangeThatTrack
+{
+    [Activity(Label = "ChangeThatTrack", MainLauncher = true, Icon = "@drawable/icon", Theme = "@android:style/Theme.Holo.Light.NoActionBar")]
+    public class MainActivity : Activity
+    {
+        private ImageButton btnPlay;
+        private ImageButton btnStop;
+        private ImageButton btnNext;
+        private ImageButton btnPrev;
+
+        private ConnectionHandler conHandler;
+
+        private const int VK_MEDIA_NEXT_TRACK = 0xB0;
+        private const int VK_MEDIA_PREV_TRACK = 0xB1;
+        private const int VK_MEDIA_STOP = 0xB2;
+        private const int VK_MEDIA_PLAY_PAUSE = 0xB3;
+
+        protected override void OnDestroy()
+        {
+            conHandler.CloseConnection();
+            base.OnDestroy();
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+            conHandler = new ConnectionHandler();
+            btnPlay = FindViewById<ImageButton>(Resource.Id.btnPlay);
+        //    btnStop = FindViewById<Button>(Resource.Id.btnStop);
+            btnNext = FindViewById<ImageButton>(Resource.Id.btnNext);
+            btnPrev = FindViewById<ImageButton>(Resource.Id.btnPrev);
+
+            btnPlay.Click += BtnPlay_Click;
+          //  btnStop.Click += BtnStop_Click;
+            btnNext.Click += BtnNext_Click;
+            btnPrev.Click += BtnPrev_Click;
+            
+
+        }
+
+        private void BtnPrev_Click(object sender, System.EventArgs e)
+        {
+            conHandler.Send(VK_MEDIA_PREV_TRACK);
+        }
+
+        private void BtnNext_Click(object sender, System.EventArgs e)
+        {
+            conHandler.Send(VK_MEDIA_NEXT_TRACK);
+        }
+
+        private void BtnStop_Click(object sender, System.EventArgs e)
+        {
+            conHandler.Send(VK_MEDIA_STOP);
+        }
+
+        private void BtnPlay_Click(object sender, System.EventArgs e)
+        {
+            conHandler.Send(VK_MEDIA_PLAY_PAUSE);
+        }
+    }
+}
+
