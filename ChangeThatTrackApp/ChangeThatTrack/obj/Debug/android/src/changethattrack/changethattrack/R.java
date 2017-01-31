91c367db61c27fea5fbd9d339ea845f65908ce41
Rytis Kaplūnas <rytkaplunas@gmail.com>
2017-01-31 23:35:39 +0200

    Changed app design

--- ChangeThatTrackApp/ChangeThatTrack/obj/Debug/android/src/changethattrack/changethattrack/R.java
+++ ChangeThatTrackApp/ChangeThatTrack/obj/Debug/android/src/changethattrack/changethattrack/R.java
@@ -15,10 +15,16 @@ public final class R {
         public static final int main=0x7f020001;
         public static final int next=0x7f020002;
         public static final int nextbutton=0x7f020003;
-        public static final int pause=0x7f020004;
-        public static final int play=0x7f020005;
-        public static final int playbutton=0x7f020006;
-        public static final int prev=0x7f020007;
+        public static final int nextpressed=0x7f020004;
+        public static final int pause=0x7f020005;
+        public static final int pausebutton=0x7f020006;
+        public static final int pausepressed=0x7f020007;
+        public static final int play=0x7f020008;
+        public static final int playbutton=0x7f020009;
+        public static final int playpressed=0x7f02000a;
+        public static final int prev=0x7f02000b;
+        public static final int prevbutton=0x7f02000c;
+        public static final int prevpressed=0x7f02000d;
     }
     public static final class id {
         public static final int btnEnterIP=0x7f050001;
